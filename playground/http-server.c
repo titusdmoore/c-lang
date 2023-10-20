9daee61a3e6c4e1b4e9b214b69e4634b2848b312
Titus Moore <titusdmoore@gmail.com>
2023-10-19 20:07:36 -0400

    Added code to accept connections

--- playground/http-server.c
+++ playground/http-server.c
@@ -2,11 +2,20 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define PORT 1521 
+#define BUFFER_SIZE 1024
 
 int main() {
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  char buffer[BUFFER_SIZE];
+  char resp[] = "HTTP/1.0 200 OK\r\n"
+    "Server: webserver-c\r\n"
+    "Content-type: text/html\r\n\r\n"
+    "<html>hello, world</html>\r\n";
 
   if ( sockfd == -1 ) {
     perror("HTTP Server (socket)");
@@ -31,6 +40,30 @@ int main() {
   }
 
   printf("HTTP Server (bind, listen) success\n");
+
+  for (;;) {
+    int connsockfd = accept(sockfd, (struct sockaddr *)&addr, (socklen_t *)&addrlen);
+
+    if ( connsockfd < 0 ) {
+      perror("HTTP Server (accept)");
+      continue;
+    }
+    printf("Accepted Connection");
+
+    // Read data from requests
+    int valread = read(connsockfd, buffer, BUFFER_SIZE);
+    if ( valread < 0 ) {
+      perror("HTTP Server (read)");
+      continue;
+    }
+
+    if ( write(connsockfd, resp, strlen(resp)) < 0 ) {
+      perror("HTTP Server (write)");
+      continue;
+    }    
+
+    close(connsockfd);
+  }
 
   return 0;
 }
